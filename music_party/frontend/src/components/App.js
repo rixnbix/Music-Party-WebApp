ed9534e53e34b15ca8aea8749688cf4e38dd9f66
Ricardo <fernandesricardo121@gmail.com>
2024-06-15 18:19:13 -0700

    Added CreateRoom, RoomJoin and Homepage components to frontend and added router with route paths to the pages

--- music_party/frontend/src/components/App.js
+++ music_party/frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { createRoot } from 'react-dom/client';
+import HomePage from "./HomePage";
+
 
 export default class App extends Component {
     constructor(props) {
@@ -7,7 +9,10 @@ export default class App extends Component {
     }
 
     render() {
-        return <h1>Testing React Code</h1>
+        return (<div>
+            < HomePage />
+        </div>
+        );
     }
 }
 
