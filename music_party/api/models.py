2838a32bedfa4a3c9cf62f95e487a6e3ceb473fb
Ricardo <fernandesricardo121@gmail.com>
2024-06-17 11:13:07 -0700

    Handled post requests and updated front end for Create Room Page

--- music_party/api/models.py
+++ music_party/api/models.py
@@ -5,21 +5,19 @@ import random
 
 def generate_unique_code():
     length = 6
-    
-    while True:
-        code = ''.join(random.choices(string.ascii_uppercase, k=length)) # Concatanates 6 random uppercase alphabets
-        if Room.objects.filter(code=code).count() == 0: # Checks if any of the objects of Room have the same code 
-            break
-        
-    return code    
-        
 
-# Create your models here.
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
+
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True) # Unique code for room
-    host = models.CharField(max_length=50, unique=True) # Room can have atmost 1 host
-    guest_can_pause = models.BooleanField(null=False, default=False) # Permission for guest to pause
-    votes_to_skip = models.IntegerField(null=False, default=1) # Votes to skip song
-    created_at = models.DateTimeField(auto_now_add=True) # Date and time when created
-    
+    code = models.CharField(
+        max_length=8, default=generate_unique_code, unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
